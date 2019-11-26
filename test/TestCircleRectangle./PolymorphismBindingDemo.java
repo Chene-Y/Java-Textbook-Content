4697141c1b952afc098cfcc2316643464ca25bbc
Chene-Y <54834658+Chene-Y@users.noreply.github.com>
2019-11-26 16:01:15 +0800

    Create PolymorphismBindingDemo.java

---
+++ test/TestCircleRectangle./PolymorphismBindingDemo.java
@@ -0,0 +1,23 @@
+public class PolymorphismBindingDemo {
+	public static void main(String[] args) {
+		m(new GradeStudent());
+		m(new Student());
+		m(new Person());
+		m(new Object());
+	}
+public static void m(Object x){
+	System.out.println(x.toString());
+}
+}
+class GradeStudent extends Student{
+}
+class Student extends Person{
+public String toString(){
+	return "Student";
+}
+}
+class Person extends Object{
+	public String toString(){
+		return"Person";
+	}
+}
